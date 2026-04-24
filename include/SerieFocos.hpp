a740066e9b0570f0bb5c2b9e6e9e9820c8cdedf6
adrianceti <a25110239@ceti.mx>
2026-04-24 01:40:39 +0000

    practica 4 serie

---
+++ include/SerieFocos.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <Foco.hpp>
+
+class SerieFocos
+{
+private:
+    Foco focos[10]
+public:
+    SerieFocos(){};
+    ~SerieFocos(){};
+    void MostrarFocos(){
+        for (auto&&f : focos)
+        {
+            std::cout<<f.LeerEstado()
+        }
+        std::cout<< std ::endl;
+    }
+};
